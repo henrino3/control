e2be01438e955d11d41c6ff63e5a68ea22a46b54
Henry Mascot <henrino3@gmail.com>
2015-11-27 20:51:40 +0000

    Add a migration

--- app/views/admins/index.json.jbuilder
+++ app/views/admins/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@admins) do |admin|
-  json.extract! admin, :id, :username, :password, :name, :secuirity_question, :secuirity_answer, :email, :phone, :level
+  json.extract! admin, :id, :username, :password, :name, :security_question, :security_answer, :email, :phone, :level
   json.url admin_url(admin, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119162725) do
+ActiveRecord::Schema.define(version: 20151127195533) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20151119162725) do
     t.integer  "account_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "token"
   end
 
   add_index "banks", ["account_id"], name: "index_banks_on_account_id"
